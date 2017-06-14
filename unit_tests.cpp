726c0fb83bf55758910bbc6ee4604944742ffba9
Colin Quirke <coquirke@cisco.com>
2017-06-14 23:33:02 +0100

    Literal elimination from clauses added

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -1,25 +1,68 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
-class CNFClause
+#include <list>
+
+namespace cnf{
+
+class Clause
 {
 public:
-	CNFClause(std::vector<int> c):clause({c}){}
+    Clause(std::list<int> c):clause({c}){}
 
-	size_t size()
-	{
-		return clause.size();
-	}
+    const size_t size()
+    {
+        return clause.size();
+    }
+
+    void setVariable(int variable, bool toValue)
+    {
+        if(toValue)
+            variable *= -1;
+        clause.remove(variable);
+    }
+
 private:
-	std::vector<int> clause;
+    std::list<int> clause;
 };
+
+}
 
 TEST_CASE("Clauses")
 
 {
-	auto clause = std::make_shared<CNFClause>(std::vector<int>({1,-2,3}));
-	SECTION("Size")
-	{
-		REQUIRE(clause->size()==3);
-	}
+    auto clause = std::make_shared<cnf::Clause>(std::list<int>({1,-2,3}));
+    SECTION("Initial Size")
+    {
+        REQUIRE(clause->size()==3);
+    }
+    SECTION("Literal Assigned")
+    {
+        SECTION("Literal not in clause")
+        {
+            clause->setVariable(4,false);
+            REQUIRE(clause->size()==3);
+        }
+        SECTION("Literal assigned false while true in clause")
+        {
+            clause->setVariable(1,false);
+            REQUIRE(clause->size()==2);
+        }
+        SECTION("Literal assigned true while false in clause")
+        {
+            clause->setVariable(2,true);
+            REQUIRE(clause->size()==2);
+        }
+        SECTION("Literal assigned false while false in clause")
+        {
+            clause->setVariable(2,false);
+            REQUIRE(clause->size()==3);
+        }
+        SECTION("Literal assigned true while true in clause")
+        {
+            clause->setVariable(1,true);
+            REQUIRE(clause->size()==3);
+        }
+
+    }
 }
