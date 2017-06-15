4cd3324f9786ef74666443323dfefbac2423f093
Colin Quirke <coquirke@cisco.com>
2017-06-15 23:48:09 +0100

    Initialistaion and satisfaction checks

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -8,20 +8,38 @@ namespace cnf{
 class Clause
 {
 private:
-    int getLFalseLiteral(int atom, bool toValue)
+    int translasteAtomAssignmentToLiteral(int atom, bool toValue)
     {
-        return toValue ? (-1*atom) : atom;
+        return toValue ? atom : (-1*atom);
     }
 
     void eliminateFalseLiteral(int atom, bool toValue)
     {
-        auto literal = getLFalseLiteral(atom,toValue);
+        auto literal = translasteAtomAssignmentToLiteral(atom, !toValue);
         literals.remove(literal);
+    }
+
+    void checkIfSatisfiedByAssignment(int atom, bool toValue)
+    {
+        auto literal = translasteAtomAssignmentToLiteral(atom, toValue);
+        satisfied = std::find(literals.begin(), literals.end(), literal) != literals.end();
     }
 
 public:
 
-    Clause(std::list<int> c):literals({c}){}
+    Clause(std::list<int> c):literals({c})
+    {
+        trim();
+    }
+
+    void trim()
+    {
+        literals.sort([](int first, int second){return abs(first) < abs(second);});
+        literals.unique();
+        auto before = literals.size();
+        literals.unique([](int first, int second){return abs(first) == abs(second);});
+        satisfied = before != literals.size();
+    }
 
     const size_t size()
     {
@@ -30,51 +48,119 @@ public:
 
     void setAtom(int atom, bool toValue)
     {
-        eliminateFalseLiteral(atom,toValue);
+        eliminateFalseLiteral(atom, toValue);
+        checkIfSatisfiedByAssignment(atom, toValue);
+    }
+
+    bool isEmpty()
+    {
+        return size() == 0;
+    }
+
+    bool isUnitClause()
+    {
+        return size() == 1;
+    }
+
+    bool isSatisfied()
+    {
+        return satisfied;
     }
 
 private:
 
     std::list<int> literals;
+    bool satisfied = false;
 };
 
 }
 
 TEST_CASE("Clauses")
-
 {
-    auto clause = std::make_shared<cnf::Clause>(std::list<int>({1,-2,3}));
-    SECTION("Initial Size")
+    SECTION("Unit Clauses have one literal")
     {
-        REQUIRE(clause->size()==3);
+        auto unitClause = std::make_shared<cnf::Clause>(std::list<int>({1}));
+        REQUIRE(unitClause->isUnitClause());
+        SECTION("Unit clause becomes unsatisfiable by atom assignment")
+        {
+            unitClause->setAtom(1,false);
+            REQUIRE(unitClause->isEmpty());
+        }
+        SECTION("Unit clause satisfied by atom assignment")
+        {
+            unitClause->setAtom(1,true);
+            REQUIRE(unitClause->isSatisfied());
+        }
     }
     SECTION("Atom Assigned")
     {
-        SECTION("Atom not in clause")
+        auto clause = std::make_shared<cnf::Clause>(std::list<int>({1,-2,3}));
+        SECTION("Initial Size")
+        {
+            REQUIRE(clause->size()==3);
+        }
+        SECTION("Clause with active literals is not empty")
+        {
+            REQUIRE(!clause->isEmpty());
+        }
+        SECTION("Setting an Atom not in clause has no effect")
         {
             clause->setAtom(4,false);
             REQUIRE(clause->size()==3);
         }
-        SECTION("Atom assigned false while literal true in clause")
+        SECTION("Atom assigned false eliminates true literal")
         {
             clause->setAtom(1,false);
             REQUIRE(clause->size()==2);
         }
-        SECTION("Atom assigned true while literal false in clause")
+        SECTION("Atom assigned true eliminates false literal")
         {
             clause->setAtom(2,true);
             REQUIRE(clause->size()==2);
         }
-        SECTION("Atom assigned false while literal false in clause")
-        {
-            clause->setAtom(2,false);
-            REQUIRE(clause->size()==3);
-        }
-        SECTION("Atom assigned true while literal true in clause")
+        SECTION("Atom assigned true satisfies clause")
         {
             clause->setAtom(1,true);
+            REQUIRE(clause->isSatisfied());
+        }        
+        SECTION("Atom assigned false does not satisfy clause")
+        {
+            clause->setAtom(1,false);
+            REQUIRE(!clause->isSatisfied());
+        }
+        SECTION("Atom assigned false satisfies the clause")
+        {
+            clause->setAtom(2,false);
+            REQUIRE(clause->isSatisfied());
+        }
+        SECTION("Clause with multiple literals is not a Unit Clause")
+        {
+            REQUIRE(!clause->isUnitClause());
+            SECTION("but becomes unit through variable elimination")
+            {
+                clause->setAtom(1,false);
+                clause->setAtom(2,true);
+                REQUIRE(clause->isUnitClause()); 
+            }
+        }
+    }
+    SECTION("Initialise")
+    {
+
+        SECTION("Initial Size")
+        {
+            auto clause = std::make_shared<cnf::Clause>(std::list<int>({1,-2,3}));
             REQUIRE(clause->size()==3);
         }
-
+        SECTION("Initial Size Trimed")
+        {
+            auto clause = std::make_shared<cnf::Clause>(std::list<int>({1,-2,3,-2,1}));
+            REQUIRE(clause->size()==3);
+        }
+        SECTION("Clause that contains a literal and its negation is trivially satisfied")
+        {
+            auto clause = std::make_shared<cnf::Clause>(std::list<int>({1,-2,-1}));
+            REQUIRE(clause->isSatisfied());
+        }
     }
 }
